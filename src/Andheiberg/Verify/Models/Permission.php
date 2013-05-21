52bc1f9651c271b0a55815a868fbcb758def3376
Andreas Heiberg <git@andreas-heiberg.com>
2013-05-21 17:55:17 +0200

    cleaned permissions
    Signed-off-by: Andreas Heiberg <git@andreas-heiberg.com>

--- src/Andheiberg/Verify/Models/Permission.php
+++ src/Andheiberg/Verify/Models/Permission.php
@@ -1,28 +1,29 @@
 <?php namespace Andheiberg\Verify\Models;
 
 class Permission extends BaseModel {
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'permissions';
+	
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'permissions';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = array('name', 'description');
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = array('name');
 
-    /**
-     * Roles
-     *
-     * @return object
-     */
-    public function roles()
-    {
-        return $this->belongsToMany('Toddish\Verify\Models\Role', 'permission_role')
-        ->withTimestamps();
-    }
+	/**
+	 * Roles
+	 *
+	 * @return object
+	 */
+	public function roles()
+	{
+		return $this->belongsToMany('Toddish\Verify\Models\Role', 'permission_role')->withTimestamps();
+	}
+	
 }
